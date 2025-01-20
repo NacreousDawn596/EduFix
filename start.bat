a62625bad88cc616fc6b0896f25bd6c0ba641e2d
NacreousDawn596 <Aferiad.kamal@proton.me>
2025-01-20 21:40:10 +0100

    final

--- start.bat
+++ start.bat
@@ -2,7 +2,9 @@
 
 call .\Scripts\Activate.bat
 
-waitress-serve --listen=127.0.0.1:5002 app:app
+start "" waitress-serve --listen=127.0.0.1:5002 app:app
+
+timeout /t 1 /nobreak >nul
 
 start http://127.0.0.1:5002/
 
